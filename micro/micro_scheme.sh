911e7938f88efbea3b8c8c108ee8eefe107588df
N.Maltsev <nsmalcev@gmail.com>
2020-07-01 19:47:52 +0200

    update microconfig

--- micro/micro_scheme.sh
+++ micro/micro_scheme.sh
@@ -1,6 +1,7 @@
 # copying the colorscheme for Micro text editor
 mkdir -p ~/.config/micro/colorschemes
 cp bubblegum_mn.micro ~/.config/micro/colorschemes
+cp settings.json ~/.config/micro
 # An alias for Micro
 echo 'alias xmicro="export TERM=xterm-256color;micro $1"' >> ~/.bashrc
 source ~/.bashrc
